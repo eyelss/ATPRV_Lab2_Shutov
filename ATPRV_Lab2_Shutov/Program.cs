75ef7bb909d0d8a1f2bacce6cf9a730403f722d7
eyelss <stud0000200014@study.utmn.ru>
2023-03-14 05:51:44 +0500

    Initial commit

---
+++ ATPRV_Lab2_Shutov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<int> { 1, 2, 3 };
+
+    
+    }
+}
